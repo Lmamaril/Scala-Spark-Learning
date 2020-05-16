bc7ae3b31bba413a1d1606d3954b1ff600b74ea9
Leah <8636027+Lmamaril@users.noreply.github.com>
2020-05-16 16:19:00 -0700

    add read file test

--- HousingAnalysis.scala
+++ HousingAnalysis.scala
@@ -13,10 +13,22 @@ object Analysis {
     val description = "Housing Analysis"
     print(description)
 
+    val nums = sc.parallelize(Array(1,2,3,4,5,6,7,8,9,10))
+    nums.foreach(println)
+
     val rdd = sc.textFile("home_data.csv")
-    rdd.foreach(f=>{
-      println(f)
-    })
+    val words = rdd.flatMap(line => line.split(","))
+    val wordCount = words.map(word => (word,1)).reduceByKey(_ + _)
+    wordCount.foreach(println)
+    sc.stop()
+    
+//    // read the full file
+//    rdd.foreach(f=>{
+//      println(f)
+//    })
+
+    print("How many houses were built before 1979?")
+    print("What is the most expensive zipcode in the dataset?")
 
   }
 }
