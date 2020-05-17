13b0a1b594a6c9013284f36e951dc013a4be706e
Leah <8636027+Lmamaril@users.noreply.github.com>
2020-05-16 20:02:55 -0700

    Answered two questions

--- HousingAnalysis.scala
+++ HousingAnalysis.scala
@@ -11,34 +11,19 @@ object Analysis {
       .enableHiveSupport()
       .getOrCreate()
 
-    case class homeSchema (id: String,
-                         date: String,
-                         price: Int, bedrooms: String,
-                         bathrooms: String,
-                         sqft_living: Int,
-                         sqft_lot: Int,
-                         floors: String,
-                         waterfront: Boolean,
-                         view: Boolean,
-                         condition: Int,
-                         grade: Int,
-                         sqft_above: Int,
-                         sqft_basement: Int,
-                         yr_built: Int,
-                         yr_renovated: Int,
-                         zipcode: String,
-                         latitude: Double,
-                         longitude: Double,
-                         sqft_living15: Int,
-                         sqft_lot15: Int)
-    
-
     print("Housing Analysis")
     val df = spark.read.options(Map("inferSchema"->"true","delimiter"->",", "header"-> "true")).csv("home_data.csv")
     df.printSchema()
 
-    print("How many houses were built before 1979?")
-    print("What is the most expensive zipcode in the dataset?")
+    println("How many houses were built before 1979?")
 
+    val homesPre1979 = df.filter(df("yr_built") < 1979)
+    println("Count of houses build before 1979: ", homesPre1979.count())
+
+    println("What is the most expensive zipcode in the dataset?")
+
+    val priciest_zipcode = df.groupBy("zipcode").mean("price").orderBy(org.apache.spark.sql.functions.col("avg(price)").desc).take(1)
+    priciest_zipcode.foreach(println)
   }
 }
+
