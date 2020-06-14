ebdf7a76f37de6218b65a12ee2e0d0ba201e324f
Leah <8636027+Lmamaril@users.noreply.github.com>
2020-06-14 13:52:08 -0700

    remove lists in the values and added questions

--- NYTBookData/NYTBookData.scala
+++ NYTBookData/NYTBookData.scala
@@ -1,6 +1,6 @@
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.types._
-import org.apache.spark.sql.functions.{col, element_at}
+import org.apache.spark.sql.functions.{col, when, element_at}
 object NYTBookDataAnalysis {
 
   def main(args: Array[String]): Unit = {
@@ -14,23 +14,36 @@ object NYTBookDataAnalysis {
 
     print("NYT Book Analysis")
 
-//    val schema = new StructType()
-//      .add( new StructType("_id", StringType, true))
-//      .add("amazon_product_url", StringType, true)
-//      .add("author", StringType,true)
-//      .add("bestsellers_date",LongType,true)
-//      .add("description", StringType, true)
-//      .add("published_date", DateType, true )
-//      .add("publisher", StringType, true)
-//      .add("rank", IntegerType, true)
-//      .add("rank_last_week", StringType, true)
-//      .add("title", StringType, true)
-//      .add("weeks_on_list", IntegerType, true)
-
     val df = spark.read.json("nyt2.json").toDF()
-    df.take(5).foreach(println)
-    //df.withColumn("_id", element_at(col("_id"),0))
-    println(df.select(df("_id")).show())
+
+    // clean the dataset so that the json translates to a data frame format
+    val cleaned = df.withColumn("_id",col("_id.$oid"))
+      .withColumn("bestsellers_date",col("bestsellers_date.$date.$numberLong"))
+      .withColumn("price", when(col("price.$numberDouble").isNull, col("price.$numberInt")).otherwise(col("price.$numberDouble")).cast("Double"))
+      .withColumn("published_date", col("published_date.$date.$numberLong"))
+      .withColumn("rank", col("rank.$numberInt"))
+      .withColumn("rank_last_week", col("rank_last_week.$numberInt"))
+      .withColumn("weeks_on_list", col("weeks_on_list.$numberInt"))
+
+    // take a look at the schema
+    println(cleaned.printSchema)
+
+    // view a portion of the dataset
+    cleaned.take(5).foreach(println)
+
+    // which weeks had the top prices?
+    println(cleaned.groupBy("weeks_on_list")
+      .avg("price")
+      .orderBy(col("avg(price)").desc)
+      .show(false))
+
+    // which books are ranked top ten?
+    cleaned.select(col("title"), col("author"), col("rank"))
+      .orderBy(col("rank").asc)
+      .take(10).foreach(println)
+    
+    // count the number of free books that are within this data. what is the ratio between free
+    // and non free books?
 
 
   }
